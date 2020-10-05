b1431cd3b63d94a9272ef008958483a46608ef92
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2020-10-05 07:47:29 +0500

    Created Node header file

---
+++ Node.h
@@ -0,0 +1,28 @@
+//A node class which creates the nodes for the linked List along basic getter and setter functions
+
+#include<iostream>
+using namespace std;
+#ifndef NODE
+#define NODE
+class Node
+{
+private:
+	int data;
+	Node* next;
+	Node* prev;
+
+public:
+	Node();
+	Node(int d);			// d=data
+	void set_data(int d);
+	void set_next(Node* n);		// n=next
+	void set_prev(Node* p);		// p=prev
+	int get_data();
+	Node* get_next();
+	Node* get_prev();
+
+
+};
+
+#endif
+
