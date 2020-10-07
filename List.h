e8293eafced6a746a9041140752303d69b82cb0c
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2020-10-07 07:05:48 +0500

    List Header file

---
+++ List.h
@@ -0,0 +1,27 @@
+//Header file for the Linked List with Basic funtions
+
+#include<iostream>
+#include"Node.h"
+#ifndef LIST
+#define LIST
+using namespace std;
+class List
+{
+private:
+	Node* head = NULL;
+	Node* tail = NULL;
+
+public:
+	List();
+	int node_counter = 0;
+	bool insert(int d);		// d=data
+	bool insert_at(int d, int pos);
+	bool delete_list(int d);
+	void print();
+	int search(int d);
+	//void sort();
+	void set_head(Node* h);
+
+};
+
+#endif
